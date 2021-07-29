0814d90fe1b488381e764694b5bdc5318a581fb4
SlavaGrach <grachv77@gmail.com>
2021-07-29 20:04:10 +0300

    bugfix clear code

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ import filterContacts from './helpers/filterContacts';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
@@ -29,6 +24,10 @@ export class App extends Component {
     evt.preventDefault();
     if (this.state.contacts.some(el => el.name === this.state.name)) {
       alert(`${this.state.name} is already in contacts`);
+      this.setState(prev => ({
+        name: '',
+        number: '',
+      }));
       return;
     }
     const contact = {
